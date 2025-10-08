21d7f56a296102e8f958ebc17bf9a97dc840414a
pratikqupp <113935701+pratikqupp@users.noreply.github.com>
2025-10-08 12:19:04 +0530

    jj

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# install spaCy model if not already present
+echo "Starting deployment..."
+
+# Ensure spaCy model installed
 python3 -m spacy download en_core_web_sm || true
+
 echo "Starting mail_fetcher.py..."
-# run main script
+
+# Run your script
 python3 mail_fetcher.py
